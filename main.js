686d846e1eb2611b8bb4f3ed60566d9aa1755724
groths89-at-487435587416 <11449927+groths89@users.noreply.github.com>
2023-08-14 16:37:01 -0400

    working on portfolio page

---
+++ main.js
@@ -0,0 +1,9 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelector('.control');
+const allSections = document.querySelector('.main-content');
+
+function pageTransitions() {
+    // Button Click Actibe Class
+    
+}
